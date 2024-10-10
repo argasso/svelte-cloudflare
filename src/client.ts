060420849f22139406fe568c131904e3f8ed67ab
Robert Olsson <robbanolsson@gmail.com>
2024-10-10 21:46:07 +0200

    skip env vars

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,16 @@
 import { HoudiniClient } from '$houdini'
-import { PUBLIC_SHOPIFY_STOREFRONT_TOKEN, PUBLIC_SHOPIFY_STOREFRONT_URL } from '$env/static/public'
+// import { PUBLIC_SHOPIFY_STOREFRONT_TOKEN, PUBLIC_SHOPIFY_STOREFRONT_URL } from '$env/static/public'
 
 const headers = {
   headers: {
-    'X-Shopify-Storefront-Access-Token': PUBLIC_SHOPIFY_STOREFRONT_TOKEN,
+    // 'X-Shopify-Storefront-Access-Token': PUBLIC_SHOPIFY_STOREFRONT_TOKEN,
+    'X-Shopify-Storefront-Access-Token': 'ff151810f966c1536e2d3b4fd437f38e',
   },
 }
 
 export default new HoudiniClient({
-  url: PUBLIC_SHOPIFY_STOREFRONT_URL,
+  // url: PUBLIC_SHOPIFY_STOREFRONT_URL,
+  url: 'https://argasso.myshopify.com/api/2024-10/graphql',
   fetchParams() {
     return {
       ...headers,
