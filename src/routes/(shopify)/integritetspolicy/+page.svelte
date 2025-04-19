1de66c1fe6ab35f95143f6f83908f959806cb651
Robert Olsson <robbanolsson@gmail.com>
2025-04-19 22:52:08 +0300

    Privacy policy and admin route

---
+++ src/routes/(shopify)/integritetspolicy/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Section from '$lib/components/Section.svelte'
+
+  export let data
+</script>
+
+<Section>
+  <h1 class="mb-8 mt-0 w-full text-center md:text-left">
+    {data.title}
+  </h1>
+  {@html data.page}
+</Section>
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Section from '$lib/components/Section.svelte'
+</script>
+
+<Section>
+  <h1>Admin Page</h1>
+</Section>
